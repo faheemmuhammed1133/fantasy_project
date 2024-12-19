12b5375cfff1bc628a4563e4406dd2e277540e7e
Muhammed Faheem <142870023+faheemmuhammed1133@users.noreply.github.com>
2024-12-19 11:25:55 +0530

    Update MyMatch.js
    added timestamps

--- backend/models/MyMatch.js
+++ backend/models/MyMatch.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const myMatchSchema = new mongoose.Schema({
   teamId: { type: mongoose.Schema.Types.ObjectId, ref: 'team', required: true },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
-});
+},{timestamps:true});
 
 const MyMatch = mongoose.model('mymatches', myMatchSchema);
 export default MyMatch
