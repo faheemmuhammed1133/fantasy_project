36692470b92ef76878abd8b42822edde0c610aee
Muhammed Faheem <142870023+faheemmuhammed1133@users.noreply.github.com>
2024-12-19 11:26:26 +0530

    Update Team.js
    added timestamps

--- backend/models/Team.js
+++ backend/models/Team.js
@@ -5,7 +5,7 @@ const teamSchema = new mongoose.Schema({
   status: { type: String, enum: ['win', null], default: null },
   players: [{ type: mongoose.Schema.Types.ObjectId, ref: 'players' }],
   points: { type: Number, default: 0 },
-});
+},{timestamps:true});
 
 const Team = mongoose.model('teams', teamSchema);
 export default Team
