8a84469c7d81c20a5b7ffa48656b41f8e2463cb7
Muhammed Faheem <142870023+faheemmuhammed1133@users.noreply.github.com>
2025-03-18 11:15:45 +0530

    Delete backend directory

--- backend/routes/matchRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createMatch, getMatches } from '../controllers/matchController.js';
-
-const router = express.Router();
-
-router.post('/', createMatch);
-router.get('/', getMatches);
-
-export default router;
--- backend/routes/teamRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createTeam, getTeams } from '../controllers/teamController.js';
-
-const router = express.Router();
-
-router.post('/', createTeam);
-router.get('/', getTeams);
-
-export default router;
